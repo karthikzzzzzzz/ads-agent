716da850442de4795ced3f1a95767d307fc16b53
Karthik Rajan S <132420926+karthikzzzzzzz@users.noreply.github.com>
2025-07-01 20:27:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, UploadFile, Form
+from fastapi.responses import JSONResponse
+from app.agent_graph import final_graph
+import tempfile
+import shutil
+
+app = FastAPI()
+
+@app.post("/run-agent")
+async def run_agent(
+    csv_file: UploadFile,
+    user_query: str = Form(...),
+    original_ad: str = Form(...),
+    tone: str = Form("fun"),
+    platform: str = Form("Google")
+):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as tmp:
+        shutil.copyfileobj(csv_file.file, tmp)
+        csv_path = tmp.name
+
+    input_state = {
+        "csv_path": csv_path,
+        "user_query": user_query,
+        "original_ad": original_ad,
+        "tone": tone,
+        "platform": platform,
+    }
+
+    result = final_graph.invoke(input_state)
+    return JSONResponse(result)
